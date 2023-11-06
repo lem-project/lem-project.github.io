1b27842105e9b0af02bfc7cdb8818a99f37b6d7c
Fermin <fmfs@posteo.net>
2023-11-06 18:14:34 +0100

    Add curl command to it

--- static/linux_installer.sh
+++ static/linux_installer.sh
@@ -1,7 +1,8 @@
 # For now, this only works with SBCL
 if [ $(which sbcl) ]
 then
-    sbcl --noinform  --non-interactive --load "linux-installer.lisp" --eval "(exit)"
+    cd /tmp && wget https://lem-project.github.io/lem-page/linux-installer.lisp
+    sudo sbcl --noinform  --non-interactive --load "/tmp/linux-installer.lisp" --eval "(exit)"
  else
     error "SBCL not found on the system!"
 fi
